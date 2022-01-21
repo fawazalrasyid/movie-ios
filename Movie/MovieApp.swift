068a6403a2efd614606a9081b6bb82785af48733
fawazalrasyid <alrasyid.fawaz16@gmail.com>
2022-01-21 23:12:22 +0700

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Fawaz Al Rasyid on 21/01/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
